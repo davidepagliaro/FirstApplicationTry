f04d924dc83d0f474f66599b0dd5a4ff525c286a
davidepagliaro <dpagliaro97@gmail.com>
2017-02-10 15:07:57 +0100

    login implemented

---
+++ app/src/main/java/com/dpagl/newapplicationfirst/LoginActivity.java
@@ -0,0 +1,95 @@
+package com.dpagl.newapplicationfirst;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.Toast;
+
+/**
+ * Created by dpagl on 10/02/2017.
+ */
+
+public class LoginActivity extends Activity {
+
+    EditText usernameET;
+    EditText passwordET;
+    LinearLayout loginBT;
+
+    private boolean loginControl(String username, String password) {
+
+        if (username.equals("dpagliaro97@gmail.com") && password.equals("coglione")) {
+            return true;
+        } else {
+            return false;
+        }
+
+
+    }
+
+
+    private static final String TAG = "MainActivity";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        Log.d(TAG, "onCreate");
+
+        loginBT = (LinearLayout) findViewById(R.id.login_button);
+        loginBT.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                usernameET = (EditText) findViewById(R.id.login_email);
+                passwordET = (EditText) findViewById(R.id.login_password);
+                System.out.println(usernameET.toString());
+                if (loginControl(usernameET.getText().toString(), passwordET.getText().toString())){
+                    Toast.makeText(LoginActivity.this, getString(R.string.login), Toast.LENGTH_LONG).show();
+                    Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                    startActivity(intent);
+                    finish();
+                } else {
+
+                    Toast.makeText(LoginActivity.this, "e-mail o password errate", Toast.LENGTH_SHORT).show();
+                }
+
+            }
+        });
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d(TAG, "onStart");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.d(TAG, "onRestart");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(TAG, "onPause");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(TAG, "onResume");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG, "onDestroy");
+    }
+
+}
--- app/src/main/java/com/dpagl/newapplicationfirst/MainActivity.java
+++ app/src/main/java/com/dpagl/newapplicationfirst/MainActivity.java
@@ -1,8 +1,9 @@
 package com.dpagl.newapplicationfirst;
 
 import android.app.Activity;
+
 import android.os.Bundle;
-import android.util.Log;
+
 
 /**
  * Created by dpagl on 08/02/2017.
@@ -10,43 +11,35 @@ import android.util.Log;
 
 public class MainActivity extends Activity{
 
-    private static final String TAG="MainActivity";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
-        Log.d(TAG,"onCreate");
+        setContentView(R.layout.activity_main);
     }
 
     @Override
     protected void onStart() {
         super.onStart();
-        Log.d(TAG,"onStart");
-    }
-
-    @Override
-    protected void onRestart() {
-        super.onRestart();
-        Log.d(TAG,"onRestart");
-    }
-
-    @Override
-    protected void onPause() {
-        super.onPause();
-        Log.d(TAG,"onPause");
     }
 
     @Override
     protected void onResume() {
         super.onResume();
-        Log.d(TAG,"onResume");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
     }
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
-        Log.d(TAG,"onDestroy");
     }
 
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+    }
 
 }
